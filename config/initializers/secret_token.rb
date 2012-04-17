0d06d5f6b33cd10418608b456cc752b5ce0d4a67
Jeremy Spiegel <jeremy.s.spiegel@gmail.com>
2012-04-17 00:06:22 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rosham::Application.config.secret_token = '4ccb52d258cc6331b7d313aa2eea50fc0895219e1f0aee9c4cbadbe71cbf99c5e2b9450df0a39f4c5cb175f0864975e30ddccc4a6172b9c82f9ddaf7f13cc0f6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Rosham::Application.config.session_store :cookie_store, :key => '_rosham_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Rosham::Application.config.session_store :active_record_store
