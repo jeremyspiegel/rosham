27993996af7f30b8fcd744f7602ed00cffe6b14c
Jeremy Spiegel <jeremy.s.spiegel@gmail.com>
2012-04-17 01:09:43 -0700

    Add rosham_match routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rosham::Application.routes.draw do
+  get "rosham_match/new"
+
+  get "rosham_match/create"
+
+  get "rosham_match/show"
+
+  get "rosham_match/update"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +56,7 @@ Rosham::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'rosham_match#new'
 
   # See how all your routes lay out with "rake routes"
 
